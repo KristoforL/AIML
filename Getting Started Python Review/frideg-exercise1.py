376568bb3d1ebaa3de8446e635767d45eb7c7364
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2024-07-16 00:27:29 -0400

    making the exervise to complete later today

---
+++ Getting Started Python Review/frideg-exercise1.py
@@ -0,0 +1,15 @@
+#The Fridge
+
+"""
+    Get the user to enter temp in celsius 
+    
+    if temp <0 print fridge too cold
+    
+    if temp 0-4 degree print fridge ok
+    
+    if temp 4-6 degrees print fridge too warm
+    
+    if temp >6 print Fridge broken
+"""
+    
+    

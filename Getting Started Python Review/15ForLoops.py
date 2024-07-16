67eb9375340e2c07fee79a40fbf03ba814cc5bdc
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2024-07-16 09:03:54 -0400

    reviewing more for loops

--- Getting Started Python Review/15ForLoops.py
+++ Getting Started Python Review/15ForLoops.py
@@ -1,5 +1,10 @@
 #Review of for loops that will go for as long as a condition is not met which might not ever happen or you could break out of it after so many attempts using other conditions
 
+#Range includes 0 since computers start counting at 0. This leads to leaving off the number you want in that range so adjust accordingly
+for i in range(5):
+    print(i)
 
-for i=0, i<=5, i+=1:
+
+print("Range can have to parameters but if use two but still the ending range number is not included so account for that")
+for i in range(1,6):
     print(i)
--- Getting Started Python Review/frideg-exercise1.py
+++ Getting Started Python Review/frideg-exercise1.py
@@ -13,16 +13,40 @@
 """
     
     
+# temp = input("How hot is it in celsius degrees: ")
+
+# print("You said it was " + temp + " Degrees Celsius")
+
+# C = float(temp)
+# if float(C) < 0:
+#     print("Fridge too cold!")
+# elif float(C) >= 0 and float(C) <=4:
+#     print("Fridge is ok")
+# elif float(C) >4 and float(C) <=6:
+#     print("Fridge too warm")
+# else:
+#     print("Fridge Broken") 
+    
+    
+#Improved the fridge solution with one print statement in one place. Could utilize constants since we know these statuses will not change
+
 temp = input("How hot is it in celsius degrees: ")
 
 print("You said it was " + temp + " Degrees Celsius")
 
+STATUS_BROKEN = "Fridge is broken"
+STATUS_COLD = "Fridge is too cold"
+STATUS_WARM = "Fridge is too warm"
+STATUS_OK =  "Fridge is ok"
+
+status = STATUS_BROKEN
+
 C = float(temp)
 if float(C) < 0:
-    print("Fridge too cold!")
+    status = STATUS_COLD
 elif float(C) >= 0 and float(C) <=4:
-    print("Fridge is ok")
+    status = STATUS_OK
 elif float(C) >4 and float(C) <=6:
-    print("Fridge too warm")
-else:
-    print("Fridge Broken") 
+    status = STATUS_WARM
+    
+print(status)

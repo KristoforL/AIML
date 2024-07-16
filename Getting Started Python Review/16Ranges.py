177a61f393f2a24215dd5f575776e470a07ae510
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2024-07-16 09:43:19 -0400

    Reviewing ranges

---
+++ Getting Started Python Review/16Ranges.py
@@ -0,0 +1,6 @@
+#Review of ranges. Although lesson 15 went over it slightly here we will explore more limitations around range method
+
+
+print("Range can have to parameters but if use two but still the ending range number is not included so account for that")
+for i in range(1,6):
+    print(i)

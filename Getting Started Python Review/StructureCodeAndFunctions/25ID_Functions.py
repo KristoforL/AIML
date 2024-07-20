0a3363b0f966aac46ab7686978ced0413e24577d
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2024-07-20 00:27:01 -0400

    Almost done for the day

--- Getting Started Python Review/StructureCodeAndFunctions/25ID_Functions.py
+++ Getting Started Python Review/StructureCodeAndFunctions/25ID_Functions.py
@@ -1,1 +1,18 @@
 # Reviewing ID Functions
+
+#You ID the variables that you use in your functions to ensure the variables are different or the same
+
+def greet(name):
+    print(id(name))
+    print("Hello " + name)
+
+
+def main():
+    name = "John"
+    
+    print(id(name))
+    greet(name)
+
+
+    
+main()
